2e9f0c226df65b39e95f4b497d49eb82efb53be9
Berat Dalsuna <dalsunaberat@gmail.com>
2026-03-14 15:15:50 +0300

    feat: mobile spesific config for run game

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@
 #![allow(clippy::needless_pass_by_value)]
 
 #[cfg(target_os = "android")]
-use bevy::prelude::bevy_main;
+use bevy::{prelude::bevy_main, window::AppLifecycle};
+
+#[cfg(any(target_os = "android", target_os = "ios"))]
+use bevy::winit::WinitSettings;
+
 use bevy::prelude::*;
 
 mod combat;
@@ -32,24 +36,54 @@ fn main() {
 }
 
 pub fn run_game() {
-    App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(ImagePlugin::default_nearest())
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: "Yulaven".to_string(),
-                        ..default()
-                    }),
+    let mut app = App::new();
+
+    #[cfg(any(target_os = "android", target_os = "ios"))]
+    app.insert_resource(WinitSettings::mobile());
+
+    #[cfg(target_os = "android")]
+    app.add_systems(
+        Update,
+        handle_lifetime.run_if(any_with_component::<AudioSink>),
+    );
+
+    app.add_plugins(
+        DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Yulaven".to_string(),
+                    #[cfg(any(target_os = "android", target_os = "ios"))]
+                    resizable: false,
+                    #[cfg(any(target_os = "android", target_os = "ios"))]
+                    mode: bevy::window::WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
                     ..default()
                 }),
-        )
-        .add_plugins(core::CorePlugin)
-        .add_plugins(player::PlayerPlugin)
-        .add_plugins(enemy::EnemyPlugin)
-        .add_plugins(combat::CombatPlugin)
-        .add_plugins(map::MapPlugin)
-        .add_plugins(ui::UiPlugin)
-        .add_message::<player::components::LevelUpEvent>()
-        .run();
+                ..default()
+            }),
+    )
+    .add_plugins(core::CorePlugin)
+    .add_plugins(player::PlayerPlugin)
+    .add_plugins(enemy::EnemyPlugin)
+    .add_plugins(combat::CombatPlugin)
+    .add_plugins(map::MapPlugin)
+    .add_plugins(ui::UiPlugin)
+    .add_message::<player::components::LevelUpEvent>()
+    .run();
+}
+
+// Pause audio when app goes into background and resume when it returns.
+// This is handled by the OS on iOS, but not on Android.
+#[cfg(target_os = "android")]
+fn handle_lifetime(
+    mut app_lifecycle_reader: MessageReader<AppLifecycle>,
+    music_controller: Single<&AudioSink>,
+) {
+    for app_lifecycle in app_lifecycle_reader.read() {
+        match app_lifecycle {
+            AppLifecycle::Idle | AppLifecycle::WillSuspend | AppLifecycle::WillResume => {}
+            AppLifecycle::Suspended => music_controller.pause(),
+            AppLifecycle::Running => music_controller.play(),
+        }
+    }
 }
